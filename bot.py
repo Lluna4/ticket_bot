ebe6bf8e65bda7fbc288d533d368a3ef7fab08cc
carlyjb17 <69774917+carlyjb17@users.noreply.github.com>
2021-09-19 16:56:36 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ async def on_message(message):
                 id = channel.name[7:]
                 try:
                     ids.append(id)
+                    id = int(ids[-1]) + 1
                     
                 except Exception:
                     id = 0
@@ -30,37 +31,43 @@ async def on_message(message):
                 id = 0
 
         admins = [role for role in message.guild.roles if role.permissions.administrator] #aqui se buscan todos los roles con permiso de administrador
-        channel = await message.guild.create_text_channel(f"ticket-{int(ids[-1]) + 1}")
-        role = await message.guild.create_role(name=f"ticket-{int(ids[-1]) + 1}") #se crea el rol para la persona que lo crea
-        await message.author.add_roles(role)
-        await channel.set_permissions(role, read_messages=True, send_messages=True)
+        channel = await message.guild.create_text_channel(f"ticket-{id}")
+        renames[channel] = channel
+       
+        await channel.set_permissions(message.author, read_messages=True, send_messages=True)
         
-        for admin in admins: # todos los admins pueden leer y escribir mensajes
-            await channel.set_permissions(admin, read_messages=True, send_messages=True)
+        admins = discord.utils.get(message.guild.roles, id=888569520439037982)
+        await channel.set_permissions(admins, read_messages=True, send_messages=True)
+
+        
+        
+        
+
+ 
             
         no_admins = [role for role in message.guild.roles if not role.permissions.administrator]
         for no_admin in no_admins: # todos los admins no pueden leer y escribir mensajes
             await channel.set_permissions(no_admin, read_messages=False, send_messages=False)
     
     if message.content.startswith("-add"): # le pone el rol del ticket a la persona que se menciona
-        persona = message.mentions
-        rol = message.channel.name
-
+        persona = await message.guild.fetch_member(int(message.content[5: ]))
+        await message.channel.set_permissions(persona, read_messages=True, send_messages=True)
         
-        try:
-            rol = renames.pop(rol)
-            rol = discord.utils.get(message.guild.roles, name=rol)
-            
-        except Exception:
-            rol = discord.utils.get(message.guild.roles, name=rol)
+   
             
             
-        await persona[0].add_roles(rol)
+   
     
     if message.content == "-close": # borra el canal en el que se ponga -close
         channel = message.channel
+        try:
+            channel = renames.pop(channel)
+            await channel.delete()
+        except Exception:
+            pass
         
-        await channel.delete()
+        
+        
 
 
     
@@ -84,4 +91,4 @@ async def on_message(message):
 
 
 
-bot.run("TOKEN")
+bot.run("ODg5MTM1NTgxNzAxOTk2NTc1.YUc2Cg.WW9N7SjeKcVN79BCjfetKn_de6c")
